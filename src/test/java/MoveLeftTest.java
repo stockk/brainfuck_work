49284307371c7e964acd4a7401d3499c77228da6
Yurii <ga1damakay@gmail.com>
2017-11-14 23:05:32 +0200

    Fixed bug with moveLeft frop position whehere point = 0.

--- src/test/java/MoveLeftTest.java
+++ src/test/java/MoveLeftTest.java
@@ -8,9 +8,14 @@ public class MoveLeftTest {
     public void oneMove() {
         Memory memory = new Memory();
         assertEquals(memory.getCurrentValue(),0);
+
+        MoveRight moveRight = new MoveRight();
+        moveRight.execute(memory);
+
         MoveLeft moveLeft = new MoveLeft();
         moveLeft.execute(memory);
-        assertEquals(memory.getPointer() +1,0);
+
+        assertEquals(0,memory.getPointer());
     }
 
     @Test
@@ -18,11 +23,16 @@ public class MoveLeftTest {
         Memory memory = new Memory();
         assertEquals(memory.getCurrentValue(),0);
         MoveLeft moveLeft = new MoveLeft();
+        MoveRight moveRight = new MoveRight();
 
+        for (int i = 0; i < 10; i++) {
+            moveRight.execute(memory);
+        }
         for (int i = 0; i < 10; i++) {
             moveLeft.execute(memory);
         }
-        assertEquals(memory.getPointer()+10,00);
+
+        assertEquals(memory.getPointer(),0);
     }
 
 }
