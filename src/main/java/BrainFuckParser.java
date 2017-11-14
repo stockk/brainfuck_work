7ad49d85a4de07152f288f2b542071e0244a872a
Yurii Haidamaka <yurii.haidamaka@teamdev.local>
2017-11-14 16:05:32 +0200

    Add class Parser without impl parse() method.

---
+++ src/main/java/BrainFuckParser.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Class converts a string to a sequence of commands.
+ *
+ */
+public class BrainFuckParser {
+
+    private String brainfuckExpression;
+
+    public BrainFuckParser(String rawCode) {
+        this.brainfuckExpression = brainfuckExpression;
+    }
+
+    private ArrayList<Command> parse(){
+        return null;
+    }
+
+}
