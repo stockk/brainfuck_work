732cb620e626bd14fd9dbb591329df1afd31c549
Yurii Haidamaka <yurii.haidamaka@teamdev.local>
2017-11-14 18:47:51 +0200

    Javadoc changes in Incremet and MoveLeft classes.

--- src/main/java/Increment.java
+++ src/main/java/Increment.java
@@ -1,10 +1,11 @@
-
+/**
+ * Class that represents API for working with  operation '+' in brainFuck language;
+ */
 public class Increment implements Command {
 
     @Override
     public void execute(Memory memory) {
         memory.increase();
     }
-
 
 }
