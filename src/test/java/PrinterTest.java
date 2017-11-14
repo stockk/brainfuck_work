78cfc1afb6d47db2df9388052470f6cee63bc55d
Yurii <ga1damakay@gmail.com>
2017-11-14 23:23:18 +0200

    Fixed bag with print symbols in PrinterTest.

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,21 +9,35 @@ public class PrinterTest {
         Memory memory = new Memory();
         assertEquals(memory.getCurrentValue(),0);
 
+        Increment increment = new Increment();
+
+        for (int i = 0; i < 65; i++) {
+            increment.execute(memory);
+        }
+
         Printer printer = new Printer();
         printer.execute(memory);
-        System.out.println(memory.getOutputString());
-        assertEquals(memory.getOutputString(),"0");
+
+        assertEquals(memory.getOutputString(),"A");
     }
 
     @Test
     public void tenPrints() {
+
         Memory memory = new Memory();
         assertEquals(memory.getCurrentValue(),0);
         Printer printer = new Printer();
+
+        Increment increment = new Increment();
+
+        for (int i = 0; i < 65; i++) {
+            increment.execute(memory);
+        }
+
         for (int i = 0; i < 10; i++) {
             printer.execute(memory);
         }
-        assertEquals(memory.getOutputString(),"0000000000");
+        assertEquals(memory.getOutputString(),"AAAAAAAAAA");
     }
 
 }
