b495804baf8b1877a0daf362a8422e035d486bed
Yurii Haidamaka <yurii.haidamaka@teamdev.local>
2017-11-14 16:09:28 +0200

    Add interface Command and class Memory with API.

---
+++ src/main/java/Memory.java
@@ -0,0 +1,31 @@
+
+public class Memory {
+
+    private int pointer = 0;
+    private char [] array = new char [20];
+
+    public int getCurrentValue() {
+        return this.array[this.pointer];
+    }
+
+    public void moveRight(){
+        this.pointer ++;
+    }
+
+    public void moveLeft(){
+        this.pointer --;
+    }
+
+    public void increase(){
+        this.array[this.pointer]++;
+    }
+
+    public void decries(){
+        this.array[this.pointer]--;
+    }
+
+    public void print(){
+            System.out.print(array[pointer]);
+    }
+    
+}
